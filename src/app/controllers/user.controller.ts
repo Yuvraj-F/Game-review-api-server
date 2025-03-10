046a3c0a8f56d2d3601e45d495c8aa9a286c4f47
Yuvraj Fagotra <yfa50@uclive.ac.nz>
2025-03-11 00:53:43 +1300

    -Implemented register user method -Implemented insert and getByEmail methods for user model -Created User type

--- src/app/controllers/user.controller.ts
+++ src/app/controllers/user.controller.ts
@@ -1,11 +1,47 @@
 import {Request, Response} from "express";
 import Logger from '../../config/logger';
+import {validate} from '../services/validator';
+import {hash, compare} from '../services/passwords';
+import * as schemas from '../resources/schemas.json'
+import * as User from '../models/user.model'
 
 
 const register = async (req: Request, res: Response): Promise<void> => {
+    Logger.info(`Registering user`);
+
+    // validate request body
+    const validation = await validate(
+        schemas.user_register,
+        req.body
+    );
+    if (validation !== true) {
+        res.statusMessage = `Bad Request: ${validation.toString()}`;
+        res.status(400).send();
+        return;
+    }
+
+    // check email already exists in database
+    const email = req.body.email;
     try {
-        res.statusMessage = "Not Implemented";
-        res.status(501).send();
+        const result = await User.getByEmail(email);
+        if (result.length !== 0) {
+            res.statusMessage = `Email already in use: ${email}`;
+            res.status(403).send();
+        }
+    } catch (err) {
+        Logger.error(err);
+        res.statusMessage = `Could not retrieve users: ${err}`
+        res.status(500).send();
+    }
+
+    // add user to the database
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const password = await hash(req.body.password);
+    try {
+        const result = await User.insert(email, firstName, lastName, password)
+        res.statusMessage = "Created";
+        res.status(201).send({"userId": result.insertId});
     } catch (err) {
         Logger.error(err);
         res.statusMessage = "Internal Server Error";
---
+++ src/app/models/user.model.ts
@@ -0,0 +1,50 @@
+import Logger from "../../config/logger";
+import {ResultSetHeader} from "mysql2";
+import {getPool} from "../../config/db";
+
+const insert = async(email:string, firstName:string, lastName:string, password:string): Promise<ResultSetHeader> => {
+    Logger.info(`Adding user ${firstName} ${lastName} to database`);
+
+    const query = `insert into user (email, first_name, last_name, password) values (?,?,?,?)`;
+    try {
+        const [result] = await getPool().query(query, [email, firstName, lastName, password]);
+        return result;
+    } catch (err) {
+        Logger.error(err.sql);
+        throw err;
+    }
+}
+
+const getOne = async(): Promise<User[]> => {
+    Logger.info(``);
+    return;
+}
+
+const getAll = async(): Promise<User[]> => {
+    Logger.info(`Getting all users from the database`);
+
+    const query = `select * from user`;
+    try {
+       const [rows] = await getPool().query(query);
+       return rows;
+    } catch (err) {
+        Logger.error(err.sql);
+        throw err;
+    }
+}
+
+const getByEmail = async(email:string): Promise<User[]> => {
+    Logger.info(`Getting user by email: ${email}`);
+
+    const query = `select * from user where email=?`;
+    try {
+        const [user] = await getPool().query(query, email);
+        return user;
+    } catch (err) {
+        Logger.error(err.sql);
+        throw err;
+    }
+}
+
+
+export{insert, getOne, getAll, getByEmail};
