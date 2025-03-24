fc247c999f4711b3bc74802387e5d031f3fc6684
Yuvraj Fagotra <yfa50@uclive.ac.nz>
2025-03-24 16:51:30 +1300

    saving progress

--- src/app/models/game.review.model.ts
+++ src/app/models/game.review.model.ts
@@ -20,6 +20,49 @@ const get = async(gameId:number): Promise<Review[]> => {
     }
 }
 
+const insert = async(gameId:number, reviewerId:number, rating:number, review:string): Promise<ResultSetHeader> => {
+    Logger.info(`Posting new review for game ${gameId}`);
+
+    let query;
+    const values =[];
+
+    // add required values
+    values.push(gameId);
+    values.push(reviewerId);
+    values.push(rating);
+
+    // only insert review if not empty
+    if (review === "") {
+        query = `insert into game_review (game_id, user_id, rating, timestamp)`+
+            ` values (?,?,?,CURRENT_TIMESTAMP)`;
+    } else {
+        query = `insert into game_review (game_id, user_id, rating, review, timestamp)`+
+            ` values (?,?,?,?,CURRENT_TIMESTAMP)`;
+        values.push(review);
+    }
+
+    try {
+        const [rows] = await getPool().query(query, values);
+        return rows;
+    } catch (err) {
+        Logger.error(err.sql);
+        throw err;
+    }
+}
+
+const hasReviewed = async(gameId:number, userId:number): Promise<{hasReviewed:number}[]> => {
+    Logger.info(`checking if user ${userId} has already reviewed game ${gameId}`);
+
+    const query = `select exists(select 1 from game_review where game_id=? and user_id=?) as hasReviewed`;
+    try {
+        const [rows] = await getPool().query(query);
+        return rows;
+    } catch (err) {
+        Logger.error(err.sql);
+        throw err;
+    }
+}
+
 const template = async(): Promise<void> => {
     Logger.info(``);
 
@@ -33,4 +76,4 @@ const template = async(): Promise<void> => {
     }
 }
 
-export{get}
+export{get, insert, hasReviewed}
